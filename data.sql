9fbc84b9625359f50d3fd8159999dc7c2cbd1e69
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2022-12-16 00:55:36 -0300

    multiple-tables

--- data.sql
+++ data.sql
@@ -1,14 +1,112 @@
-/* Populate database with sample data. */
+/* Project - create animals table. */
 
-insert into animals values (1, 'Agumon', '2020-02-03', 0, true, 10.23);
-insert into animals values (2, 'Gabumon', '2018-11-15', 2, true, 8);
-insert into animals values (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
-insert into animals values (4, 'Devimon', '2017-05-12', 5, true, 11);
+INSERT INTO animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES ('Agumon', '2020-02-03', 0, true, 10.23);
+INSERT INTO animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES ('Gabumon', '2018-11-15', 2, true, 8);
+INSERT INTO animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
+INSERT INTO animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES ('Devimon', '2017-05-12', 5, true, 11);
 
-INSERT INTO animals(id, name, date_of_birth, escape_attemps, neutered, weight_kg) values(5, 'Charmander', '2020-02-08', 0, false, -11);
-INSERT INTO animals(id, name, date_of_birth, escape_attemps, neutered, weight_kg) values(6, 'Plantmon', '2021-11-15', 2, true, -5.7);
-INSERT INTO animals(id, name, date_of_birth, escape_attemps, neutered, weight_kg) values(7, 'Squirtle', '1993-04-02', 3, false, -12.13);
-INSERT INTO animals(id, name, date_of_birth, escape_attemps, neutered, weight_kg) values(8, 'Angemon', '2005-06-12', 1, true, -45);
-INSERT INTO animals(id, name, date_of_birth, escape_attemps, neutered, weight_kg) values(9, 'Boarmon', '2005-06-07', 7, true, 20.4);
-INSERT INTO animals(id, name, date_of_birth, escape_attemps, neutered, weight_kg) values(10, 'Blossom', '1998-10-13', 3, true, 17);
-INSERT INTO animals(id, name, date_of_birth, escape_attemps, neutered, weight_kg) values(11, 'Ditto', '2022-05-14', 4, true, 22);
+/* Project - query and update animals table */
+
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Charmander', '2020-02-08', 0, false, -11);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Plantmon', '2021-11-15', 2, true, -5.7);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Squirtle', '1993-04-02', 3, false, -12.13);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Angemon', '2005-06-12', 1, true, -45);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Boarmon', '2005-06-07', 7, true, 20.4);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Blossom', '1998-10-13', 3, true, 17);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Ditto', '2022-05-14', 4, true, 22);
+
+/* Project - multiple tables */
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age)
+VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age)
+VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age)
+VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age)
+VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age)
+VALUES ('Jodie Whittaker', 38);
+INSERT INTO species (name)
+VALUES ('Pokemon');
+INSERT INTO species (name)
+VALUES ('Digimon');
+UPDATE animals
+SET species_id = (
+        SELECT id
+        FROM species
+        WHERE name = 'Digimon'
+    )
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species_id = (
+        SELECT id
+        FROM species
+        WHERE name = 'Pokemon'
+    )
+WHERE name NOT LIKE '%mon';
+UPDATE animals
+SET owner_id = (
+        SELECT id
+        FROM owners
+        WHERE full_name = 'Sam Smith'
+    )
+WHERE name = 'Agumon';
+UPDATE animals
+SET owner_id = (
+        SELECT id
+        FROM owners
+        WHERE full_name = 'Jennifer Orwell'
+    )
+WHERE name in ('Gabumon', 'Pikachu');
+UPDATE animals
+SET owner_id = (
+        SELECT id
+        FROM owners
+        WHERE full_name = 'Bob'
+    )
+WHERE name in ('Devimon', 'Plantmon');
+UPDATE animals
+SET owner_id = (
+        SELECT id
+        FROM owners
+        WHERE full_name = 'Melody Pond'
+    )
+WHERE name in ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals
+SET owner_id = (
+        SELECT id
+        FROM owners
+        WHERE full_name = 'Dean Winchester'
+    )
+WHERE name in ('Angemon', 'Boarmon');
--- queries.sql
+++ queries.sql
@@ -33,3 +33,21 @@
  SELECT name, escape_attemps FROM animals WHERE escape_attemps = (SELECT MAX(escape_attemps) FROM animals);
  SELECT species, MIN(weight_kg), MAX(weight_kg) from animals GROUP BY species;
  SELECT species, AVG(escape_attemps) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' and '2000-12-31' GROUP BY species;
+
+/* Project - query multiple tables */
+
+SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id WHERE o.full_name = 'Melody Pond';
+SELECT a.name FROM animals a JOIN species s ON a.species_id = s.id WHERE s.name = 'Pokemon';
+SELECT o.full_name, a.name FROM owners o LEFT JOIN animals a ON o.id = a.owner_id;
+SELECT count(*), s.name FROM animals a JOIN species s ON a.species_id = s.id GROUP BY s.name;
+SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id JOIN species s ON a.species_id = s.id WHERE s.name = 'Digimon' AND o.full_name = 'Jennifer Orwell';
+SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id WHERE a.escape_attempts = 0 AND o.full_name = 'Dean Winchester';
+SELECT combined.full_name FROM (SELECT o.full_name, COUNT (a.name) AS animal_number FROM owners o LEFT JOIN animals a ON o.id = a.owner_id GROUP BY o.full_name) AS combined WHERE combined.animal_number = (        SELECT MAX (animal_number)
+        FROM (
+                SELECT o.full_name,
+                COUNT (a.name) AS animal_number
+                FROM owners o
+                LEFT JOIN animals a ON o.id = a.owner_id
+                GROUP BY o.full_name
+              ) AS xx
+);
